d23443e7af232aaaa0534c234bd21face300d8c4
Felipe ALCM <felipealcmagalhaes@gmail.com>
2023-01-19 14:31:26 -0300

    Ajuste para merge na main

--- Xadrez/Models/Board.cs
+++ Xadrez/Models/Board.cs
@@ -45,6 +45,7 @@ namespace Xadrez.Models
             Console.WriteLine("  0  1  2  3  4  5  6  7");
             for (int i = 0; i < 8; i++)
             {
+                Console.ForegroundColor = ConsoleColor.White;
                 Console.Write($"{i}");
                 for (int j = 0; j < 8; j++)
                 {
